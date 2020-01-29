569bf17049e8e85f4496affed78e3db9bbf9e870
rachelwthomas <58479626+rachelwthomas@users.noreply.github.com>
2020-01-29 15:56:50 -0500

    javascript placeholder is functioning

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 
 
 
-
 let submitButton = (event) => {
 
-  let answer = "Just kidding, we don't care";
+  let answer = "Just kidding, we don't care"
 
-  // let displayResponse = document.querySelector('.response').innerHTML = answer;
+  let displayResponse = document.querySelector('.message').value = answer;
 
-  // return displayResponse;
-  return document.querySelector('.response').textContent = answer;
+  return displayResponse;
+
+
   // return alert(answer);
 
+
+
 }
+
+
 
 
 // selects the message box where you put a message
 
 
 // event listener for clicking of sumbit button
-document.querySelector('.submit-button').addEventListener("click",submitButton);
+let value = document.querySelector('.submit-button').addEventListener("click",submitButton);
