c6d2f6d8cb6474a023684d33ee7f43017850d242
RamonaEJSpence <ramonaejspence@gmail.com>
2020-01-29 20:21:24 -0500

    added another event listener because I don't know what else to do lol. I wanted to make a fizzbuzz form but instead...well, click the button lol

--- main.js
+++ main.js
@@ -16,11 +16,20 @@ let submitButton = (event) => {
 
 }
 
+// ************************FizzBuzzDon'tWork***************************************
+
+let fizzHead = document.querySelector('.fizz');
+
+let doesNotWork = (event) => {
+  fizzHead.textContent = "This doesn't even work. I'm so sorry";
+}
+
 
 
 
 // selects the message box where you put a message
 
 
-// event listener for clicking of sumbit button
+// event listener for clicking of sumbit button and fizzbutton
 let value = document.querySelector('.submit-button').addEventListener("click",submitButton);
+let fizzbuzz = document.querySelector('.fizz_button').addEventListener('click',doesNotWork);
